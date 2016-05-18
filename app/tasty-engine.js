fea2167e38e1aa32c2029b4d5548ecd1080bafd4
Antoine Stalin <antoine.stalin@gmail.com>
2016-05-18 22:43:37 +0200

    Basic Form to execute selenium input code
    1. start web driver
    2. execute input code
    3. stop web driver

---
+++ app/tasty-engine.js
@@ -0,0 +1,29 @@
+var webdriver = require('selenium-webdriver'),
+     By = webdriver.By,
+     until = webdriver.until;
+
+var driver;
+
+exports.start = function start() {
+    driver = new webdriver.Builder().
+        withCapabilities(webdriver.Capabilities.chrome()).
+        build();
+}
+
+exports.stop = function stop() {
+    driver.quit();
+}
+
+exports.execute = function execute(codeToExecute) {
+    eval(codeToExecute);
+}
+
+    //eval(executeString);
+
+
+//driver.get('http://www.wingify.com');
+/*driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('webdriver');
+driver.findElement(By.name('btnG')).click();
+driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+driver.quit();*/
